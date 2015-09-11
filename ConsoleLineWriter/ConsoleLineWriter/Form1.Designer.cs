23ab683d3a2184ccdca61699e1e9408d811d331a
Crogar <juan.t.chen@gmail.com>
2015-09-11 14:51:34 -0400

    Network Application Test in C# using native library.

---
+++ ConsoleLineWriter/ConsoleLineWriter/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ConsoleLineWriter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listen = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.getIPAdress = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PacketServer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "PacketOnly";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 67);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(268, 194);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "ServerOnly";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listen
+            // 
+            this.listen.Location = new System.Drawing.Point(12, 267);
+            this.listen.Name = "listen";
+            this.listen.Size = new System.Drawing.Size(75, 23);
+            this.listen.TabIndex = 5;
+            this.listen.Text = "Listener";
+            this.listen.UseVisualStyleBackColor = true;
+            this.listen.Click += new System.EventHandler(this.listen_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 334);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Packets";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 293);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "IP Address";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(93, 267);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Tcp";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(174, 267);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "TcpServer";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // getIPAdress
+            // 
+            this.getIPAdress.Location = new System.Drawing.Point(174, 293);
+            this.getIPAdress.Name = "getIPAdress";
+            this.getIPAdress.Size = new System.Drawing.Size(75, 23);
+            this.getIPAdress.TabIndex = 11;
+            this.getIPAdress.Text = "get IP Address";
+            this.getIPAdress.UseVisualStyleBackColor = true;
+            this.getIPAdress.Click += new System.EventHandler(this.getIPAdress_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 455);
+            this.Controls.Add(this.getIPAdress);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listen);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button listen;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button getIPAdress;
+    }
+}
+
---
+++ ConsoleLineWriter/ConsoleLineWriter/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace ConsoleLineWriter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UdpClient udpClient = new UdpClient(11000);
+            try
+            {
+                Console.WriteLine("Trying again");
+
+                IPAddress serverAddr = IPAddress.Parse("127.0.0.1");
+                udpClient.Connect(serverAddr, 11000);
+
+                Console.WriteLine("Trying again2");
+                // Sends a message to the host to which you have connected.
+                //Byte[] sendBytes = Encoding.ASCII.GetBytes("Is anybody there?");
+                Byte[] sendBytes = Encoding.ASCII.GetBytes(textBox1.Text);
+                Console.WriteLine("Trying again2-1");
+                udpClient.Send(sendBytes, sendBytes.Length);
+                Console.WriteLine("Trying again3");
+                // Sends a message to a different host using optional hostname and port parameters.
+                UdpClient udpClientB = new UdpClient();
+                udpClientB.Send(sendBytes, sendBytes.Length, "AlternateHostMachineName", 11000);
+                Console.WriteLine("Trying again4");
+                //IPEndPoint object will allow us to read datagrams sent from any source.
+
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                Console.WriteLine("Trying again5");
+                // Blocks until a message returns on this socket from a remote host.
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+                Console.WriteLine("Trying again6");
+                // Uses the IPEndPoint object to determine which of these two hosts responded.
+                Console.WriteLine("This is the message you received " +
+                                             returnData.ToString());
+                Console.WriteLine("This message was sent from " +
+                                            RemoteIpEndPoint.Address.ToString() +
+                                            " on their port number " +
+                                            RemoteIpEndPoint.Port.ToString());
+                //textBox1.Text = returnData.ToString();
+                richTextBox1.Text = returnData.ToString();
+
+                udpClient.Close();
+                udpClientB.Close();
+
+            }
+            catch (Exception f)
+            {
+                Console.WriteLine(f.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*UdpClient udpClient = new UdpClient(11000);
+            IPAddress serverAddr = IPAddress.Parse(textBox2.Text);
+            udpClient.Connect(serverAddr, 11000);*/
+
+            //141
+            //byte[] ascii = System.Text.Encoding.ASCII.GetBytes(sampleTestString);
+            //byte[] utf8 = System.Text.Encoding.UTF8.GetBytes(sampleTestString);
+
+            //byte[] pkt1426 = {
+            //     };
+
+
+            UdpClient udpClient = new UdpClient(61694);
+            //textBox2.Text = "Server Connecting too";
+            IPAddress serverAddr = IPAddress.Parse(textBox2.Text);
+            udpClient.Connect(serverAddr, 5838);
+
+            Console.WriteLine("Trying again2");
+            // Sends a message to the host to which you have connected.
+            //Byte[] sendBytes = Encoding.ASCII.GetBytes("Is anybody there?");
+            Byte[] sendBytes = Encoding.ASCII.GetBytes(textBox1.Text);
+            //Byte[] sendBytes = pkt1426;
+            Console.WriteLine("Trying again2-1");
+            int abcd = 0;
+            while(abcd < 100)
+            {
+                udpClient.Send(sendBytes, sendBytes.Length);
+                abcd++;
+            }
+            richTextBox1.Text = sendBytes.ToString();
+
+            Console.WriteLine("Trying again3");
+            // Sends a message to a different host using optional hostname and port parameters.
+
+
+            udpClient.Close();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            UdpClient udpClient = new UdpClient(11000);
+            try
+            {
+                Console.WriteLine("Trying again");
+
+                IPAddress serverAddr = IPAddress.Parse(textBox2.Text);
+                udpClient.Connect(serverAddr, 11000);
+
+                Console.WriteLine("Trying again2");
+                // Sends a message to the host to which you have connected.
+
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                Console.WriteLine("Trying again5");
+                // Blocks until a message returns on this socket from a remote host.
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+                Console.WriteLine("Trying again6");
+                // Uses the IPEndPoint object to determine which of these two hosts responded.
+                Console.WriteLine("This is the message you received " +
+                                             returnData.ToString());
+                Console.WriteLine("This message was sent from " +
+                                            RemoteIpEndPoint.Address.ToString() +
+                                            " on their port number " +
+                                            RemoteIpEndPoint.Port.ToString());
+                //textBox1.Text = returnData.ToString();
+                richTextBox1.Text = returnData.ToString();
+
+                udpClient.Close();
+
+            }
+            catch (Exception f)
+            {
+                Console.WriteLine(f.ToString());
+            }
+
+        }
+        private void listen_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = ListenForPing();
+        }
+
+        private string ListenForPing()
+        {
+            int noConnections = 0;
+            UdpClient udp = new UdpClient(11000);
+            string sendersIP;
+            while (noConnections == 0)
+            {
+
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] recData = udp.Receive(ref anyIP);
+                string ping = Encoding.ASCII.GetString(recData);
+                if (ping == "ping")
+                {
+                    Console.WriteLine("Ping received.");
+                    Console.WriteLine("Ping was sent from " + anyIP.Address.ToString() +
+                                    " on their port number " + anyIP.Port.ToString());
+                    sendersIP = anyIP.Address.ToString();
+                    //InvokePingReceiveEvent();
+                    noConnections = 1;
+                    udp.Close();
+                    return sendersIP;
+                }
+                else//send an end connection Packet like this?
+                {
+                    Console.WriteLine("Wasn't a ping");
+                    sendersIP = anyIP.Address.ToString();
+                    udp.Close();
+                    return sendersIP;
+                }
+            }
+            return "0.0.0.0";
+        }
+
+        private void Connect(String server, String message)
+        {
+            try
+            {
+                // Create a TcpClient. 
+                // Note, for this client to work you need to have a TcpServer  
+                // connected to the same address as specified by the server, port 
+                // combination.
+                Int32 port = 13000;
+                TcpClient client = new TcpClient(server, port);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing. 
+                //  Stream stream = client.GetStream();
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response. 
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+
+        private void TCPlistening()
+        {
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse(textBox2.Text);
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop. 
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests. 
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client. 
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Connect("192.168.2.19", "What the Hell?");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            TCPlistening();
+            richTextBox1.Text = "done";
+        }
+
+        private void getIPAdress_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = GetLocalIPAddress();
+        }
+
+
+    }
+}
